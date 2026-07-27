ec9744db63173efda13b82572be1cc2b5351d52b
MahendyAirlangga <mabarmabar186@gmail.com>
2026-07-27 15:42:49 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Contact from "./pages/contact";
 import Shop from "./pages/Shop";
 import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
